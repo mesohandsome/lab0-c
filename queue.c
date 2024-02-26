92f5b83006e761e35aa0a77ee9c674fa195c4b0e
mesohandsome <zxc20000317@gmail.com>
2024-02-26 16:15:46 +0800

    Fix q_new() allocation failed

--- queue.c
+++ queue.c
@@ -17,6 +17,9 @@ struct list_head *q_new()
     struct list_head *head =
         (struct list_head *) malloc(sizeof(struct list_head));
 
+    if (!head)
+        return NULL;
+
     INIT_LIST_HEAD(head);
 
     return head;
